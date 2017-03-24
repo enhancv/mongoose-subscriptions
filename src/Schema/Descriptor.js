772c38cee9b1a6b688c114383060bede26f47141
Ivan Kerin <ikerin@gmail.com>
2017-03-24 12:23:14 +0200

    Remove unneeded escapes

--- src/Schema/Descriptor.js
+++ src/Schema/Descriptor.js
@@ -9,13 +9,13 @@ const Descriptor = new Schema({
         // [3 letter company] * [up to 18 letter product]
         // [7 letter company] * [up to 14 letter product]
         // [12 letter company] * [up to 9 letter product]
-        match: /^([^\*]{3}\*.{1,18})|([^\*]{7}\*.{1,14})|([^\*]{12}\*.{1,9})$/,
+        match: /^([^*]{3}\*.{1,18})|([^*]{7}\*.{1,14})|([^*]{12}\*.{1,9})$/,
     },
     phone: {
         type: String,
         maxlength: 14,
         minlength: 10,
-        match: /[0-9\.\-]{10,14}/,
+        match: /[0-9.-]{10,14}/,
     },
     url: {
         type: String,
